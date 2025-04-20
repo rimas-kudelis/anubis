bd1835aa98ce4c27aa2468ba6670f652fdf4abae
Xe Iaso <me@xeiaso.net>
2025-04-20 14:55:46 -0400

    cryptography: add base packages for signing keys
    Signed-off-by: Xe Iaso <me@xeiaso.net>

---
+++ pkgs/cryptography/yeetfile.js
@@ -0,0 +1,29 @@
+rpm.build({
+  name: "techaro-repo-keys",
+  description: "Public keys for techaro.lol RPM packages",
+  homepage: "https://techaro.lol",
+  license: "MIT",
+  goarch: "all",
+  version: "1.0.0",
+
+  build: (out) => {
+    yeet.run(`mkdir`, `-p`, `${out}/etc/pki/rpm-gpg/techaro.lol-keys`);
+    file.install("./techaro-pkgs.pub.asc", `${out}/etc/pki/rpm-gpg/techaro.lol-keys/techaro-pkgs.asc`);
+    file.install("./techaro-root.pub.asc", `${out}/etc/pki/rpm-gpg/techaro.lol-keys/techaro-root.asc`);
+  },
+});
+
+deb.build({
+  name: "techaro-repo-keys",
+  description: "Public keys for techaro.lol RPM packages",
+  homepage: "https://techaro.lol",
+  license: "MIT",
+  goarch: "all",
+  version: "1.0.0",
+
+  build: (out) => {
+    yeet.run(`mkdir`, `-p`, `${out}/usr/share/keyrings`);
+    file.install("./techaro-pkgs.pub.asc", `${out}/usr/share/keyrings/techaro-pkgs.asc`);
+    file.install("./techaro-root.pub.asc", `${out}/usr/share/keyrings/techaro-root.asc`);
+  },
+});
