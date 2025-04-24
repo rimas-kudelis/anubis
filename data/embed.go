65cbc6922c01472e92afb5e7e72ba7e768614160
Xe Iaso <me@xeiaso.net>
2025-04-24 19:15:20 -0400

    data: add some examples of expression-based rules
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- data/embed.go
+++ data/embed.go
@@ -3,6 +3,6 @@ package data
 import "embed"
 
 var (
-	//go:embed botPolicies.yaml botPolicies.json apps bots common crawlers
+	//go:embed botPolicies.yaml botPolicies.json apps bots clients common crawlers
 	BotPolicies embed.FS
 )
