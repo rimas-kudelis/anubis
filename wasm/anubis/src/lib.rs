ea321b7f13e59120a9995284ee0d1f9c7498c06b
Xe Iaso <me@xeiaso.net>
2025-04-23 23:47:39 -0400

    fix(wasm): use wee_alloc instead of stdlib malloc
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- wasm/anubis/src/lib.rs
+++ wasm/anubis/src/lib.rs
@@ -1,5 +1,10 @@
 use std::sync::{LazyLock, Mutex};
 
+extern crate wee_alloc;
+
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 #[cfg(target_arch = "wasm32")]
 mod hostimport {
     use crate::{DATA_BUFFER, DATA_LENGTH};
