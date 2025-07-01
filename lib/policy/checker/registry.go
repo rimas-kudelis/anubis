dc0dde3053fc804cf4fe9be08c319b164a07518d
Xe Iaso <me@xeiaso.net>
2025-07-01 22:27:01 +0000

    chore: start refactor of checkers into separate packages
    Signed-off-by: Xe Iaso <me@xeiaso.net>

---
+++ lib/policy/checker/registry.go
@@ -0,0 +1,42 @@
+package checker
+
+import (
+	"encoding/json"
+	"sort"
+	"sync"
+)
+
+type Factory interface {
+	ValidateConfig(json.RawMessage) error
+	Create(json.RawMessage) (Impl, error)
+}
+
+var (
+	registry map[string]Factory = map[string]Factory{}
+	regLock  sync.RWMutex
+)
+
+func Register(name string, factory Factory) {
+	regLock.Lock()
+	defer regLock.Unlock()
+
+	registry[name] = factory
+}
+
+func Get(name string) (Factory, bool) {
+	regLock.RLock()
+	defer regLock.RUnlock()
+	result, ok := registry[name]
+	return result, ok
+}
+
+func Methods() []string {
+	regLock.RLock()
+	defer regLock.RUnlock()
+	var result []string
+	for method := range registry {
+		result = append(result, method)
+	}
+	sort.Strings(result)
+	return result
+}
