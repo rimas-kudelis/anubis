06122fcd899aed77533693f55286cd4109e076c0
Xe Iaso <me@xeiaso.net>
2025-07-18 22:07:22 -0400

    chore(devcontainer): put node_modules in a volume
    Signed-off-by: Xe Iaso <me@xeiaso.net>

--- .devcontainer/poststart.sh
+++ .devcontainer/poststart.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-pwd
+sudo chown -R vscode:vscode ./node_modules
 
 npm ci &
 go mod download &
 go install ./utils/cmd/... &
+go install mvdan.cc/sh/v3/cmd/shfmt@latest &
 
 wait
