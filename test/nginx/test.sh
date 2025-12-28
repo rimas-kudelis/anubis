d748dc9da82281bdaf163fae1891f770666a9f0a
Xe Iaso <xe.iaso@techaro.lol>
2025-12-28 18:18:25 -0500

    test: basic nginx smoke test (#1365)
    * docs: split nginx configuration files to their own directory
    
    Signed-off-by: Xe Iaso <me@xeiaso.net>
    
    * test: add nginx config smoke test based on the config in the docs
    
    Signed-off-by: Xe Iaso <me@xeiaso.net>
    
    ---------
    
    Signed-off-by: Xe Iaso <me@xeiaso.net>

---
+++ test/nginx/test.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+export VERSION=$GITHUB_COMMIT-test
+export KO_DOCKER_REPO=ko.local
+
+source ../lib/lib.sh
+
+set -euo pipefail
+
+build_anubis_ko
+mint_cert mimi.techaro.lol
+
+docker run --rm -it \
+	-v ./conf/nginx:/etc/nginx:ro \
+	-v ../pki:/techaro/pki:ro \
+	nginx \
+	nginx -t
+
+docker compose up -d
+
+docker compose down -t 1 || :
+docker compose rm -f || :
+
+exit 0
